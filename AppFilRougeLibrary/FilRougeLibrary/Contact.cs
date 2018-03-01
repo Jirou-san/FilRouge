1c32d7c049405b333251ff37c7093bb6c1283f9a
Fouque Nicolas <n.fouque@outlook.fr>
2018-03-01 11:25:47 +0100

    Namespace

--- AppFilRougeLibrary/FilRougeLibrary/Contact.cs
+++ AppFilRougeLibrary/FilRougeLibrary/Contact.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Quizz
+namespace FilRougeLibrary
 {
     /// <summary>
     /// Classe Contact permettant de créer soit un Admin soit un Agent.
