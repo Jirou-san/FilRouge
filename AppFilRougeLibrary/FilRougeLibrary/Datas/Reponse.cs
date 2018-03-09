005f6fc6d0ce9dcc166453168db55935e7d5962d
Fouque Nicolas <n.fouque@outlook.fr>
2018-03-09 15:27:55 +0100

    f

---
+++ AppFilRougeLibrary/FilRougeLibrary/Datas/Reponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilRouge.Library.Datas
+{
+    class Reponse
+    {
+        #region Properties
+        private int _ReponseId;
+        private string _Content;
+        private int _QuestionId;
+        #endregion
+        #region Accesseurs
+        public int ReponseId { get => _ReponseId; set => _ReponseId = value; }
+        public string Content { get => _Content; set => _Content = value; }
+        public int QuestionId { get => _QuestionId; set => _QuestionId = value; }
+        #endregion
+    }
+}
