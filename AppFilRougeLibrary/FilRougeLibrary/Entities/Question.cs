7c6583e47268bb57fc53f14213973cd48d9b10b9
David <david.zippari@gmail.com>
2018-04-03 09:37:16 +0200

    32

---
+++ AppFilRougeLibrary/FilRougeLibrary/Entities/Question.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FilRouge.Model.Entities
+{
+    public partial class Question
+    {
+        #region Properties
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int QuestionId { get; set; }
+        public string Content { get; set; }
+        public string Commentaire { get; set; }
+        public bool Active { get; set; }
+        //Clés étrangères
+        [ForeignKey("TypeQuestion")]
+        public int QuestionTypeId { get; set; }
+        [ForeignKey("Technology")]
+        public int TechnologyId { get; set; }
+        [ForeignKey("Difficulty")]
+        public int DifficultyId { get; set; }
+        #endregion
+        #region Association
+
+        public virtual ICollection<Quizz> Quizzs { get; set; }
+        public virtual ICollection<Reponses> Reponses { get; set; }
+        public virtual Technology Technology{ get; set; }
+        public virtual Difficulty Difficulty{ get; set; }
+        public virtual TypeQuestion TypeQuestion { get; set; }
+        #endregion
+    }
+}
