6b2bace982aa5fbdad7106924fb2016b4eb3d283
Fouque Nicolas <n.fouque@outlook.fr>
2018-04-10 15:11:03 +0200

    Ajout des annotations Lenght pour les ViewModels

--- AppFilRougeLibrary/FilRouge.Web/Models/QuizzModel.cs
+++ AppFilRougeLibrary/FilRouge.Web/Models/QuizzModel.cs
@@ -3,13 +3,12 @@ using FilRouge.Model.Entities;
 
 namespace FilRouge.Model.Models
 {
-    using System;
     using System.ComponentModel.DataAnnotations;
 
     public class QuizzModel
     {
         public int StateQuizz { get; set; }
-        public string Technology { get; set; }
+        public int TechnologyId { get; set; }
         public int QuestionCount { get; set; }
         [DisplayName("Lastname")]
         [MinLength(1)]
@@ -19,7 +18,6 @@ namespace FilRouge.Model.Models
         [MinLength(1)]
         [MaxLength(20)]
         public string UserFirstname { get; set; } // Prénom
-        public DateTime DateQuizz { get; set; }
     }
     //Classe partielle Map servant à passer d'un viewModel à un Model et inversement
     public static partial class Map
@@ -35,11 +33,11 @@ namespace FilRouge.Model.Models
             QuizzModel = new QuizzModel
              {
                  QuestionCount = Quizz.QuestionCount,
-                 Technology = Quizz.Technology.TechnoName,
+                 TechnologyId = Quizz.TechnologyId,
                  UserFirstname = Quizz.UserFirstName,
-                 UserLastname = Quizz.UserLastName,
-                 //DateQuizz = Quizz.
-            };
+                 UserLastname = Quizz.UserLastName
+                    
+             };
             return QuizzModel;
 
         }
@@ -56,7 +54,8 @@ namespace FilRouge.Model.Models
             {
                 UserLastName = QuizzModel.UserLastname,
                 UserFirstName = QuizzModel.UserFirstname,
-                QuestionCount = QuizzModel.QuestionCount
+                QuestionCount = QuizzModel.QuestionCount,
+                TechnologyId = QuizzModel.TechnologyId
             };
             return Quizz;
         }
