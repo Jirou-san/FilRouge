00e655d186ba5983bf96bd629c6e1f44834e3265
Fouque Nicolas <n.fouque@outlook.fr>
2018-04-16 11:19:24 +0200

    Mise à jour complète

--- AppFilRougeLibrary/FilRouge.Web/Models/QuestionModel.cs
+++ AppFilRougeLibrary/FilRouge.Web/Models/QuestionModel.cs
@@ -3,72 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel;
-using FilRouge.Model.Entities;
 
-namespace FilRouge.Model.Models
+namespace FilRouge.Entities.Model
 {
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
+
+    using FilRouge.Model.Entities;
 
     public class QuestionModel
     {
         public int QuestionId { get; set; }
-        //[Display(Name = "Question", Prompt = "Entrez une question", Description = "Question unique ou multiple")]
-        [DisplayName("Question")]
-        [MinLength(1)]
-        [MaxLength(250)]
-        //Aussi annoté dans le code first de la base de données
+        [DisplayName("Question:")]
         public string Content { get; set; }
         public string Comment { get; set; }
-        public string Type { get; set; }
-        public List<Response> Reponses { get; set; }
-
+        public bool Active { get; set; }
     }
-    //Classe partielle Map servant à passer d'un viewModel à un Model et inversement
+
     public static partial class Map
     {
-        public static QuestionModel MapToQuestionViewModelFull(this Question question)
-        {
-            var questionVM = new QuestionModel();
-
-            if (question == null)
-                return questionVM;
-
-            questionVM.Reponses = new List<Response>();
-
-            questionVM.QuestionId = question.QuestionId;
-            questionVM.Content = question.Content;
-            questionVM.Reponses = question.Responses.ToList();
-            questionVM.Type = question.QuestionTypeId.ToString();
-
-            return questionVM;
-        }
-
         public static QuestionModel MapToQuestionModel(this Question question)
         {
-            var questionVM = new QuestionModel();
-
+            var questionModel = new QuestionModel();
             if (question == null)
-                return questionVM;
+            {
+                return questionModel;
+            }
 
-            questionVM.QuestionId = question.QuestionId;
-            questionVM.Content = question.Content;
-            questionVM.Type = question.TypeQuestion.NameType;
-            return questionVM;
+            questionModel = new QuestionModel
+             {
+                    Content = question.Content
+             };
+            return questionModel;
         }
 
-
-
-        public static Question MapToQuestion(this QuestionModel questionVM)
+        public static Question MapToQuestion(this QuestionModel questionModel)
         {
             var question = new Question();
-            if (questionVM == null)
+            if (questionModel == null)
             {
                 return question;
             }
-            question.Content = questionVM.Content;
-            question.QuestionTypeId = Int32.Parse(questionVM.Type);
 
+            question = new Question
+                                {
+                                    Content = questionModel.Content
+            };
             return question;
         }
     }
