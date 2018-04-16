040a514b287853af497c7b4c6127534ec40fd80e
Fouque Nicolas <n.fouque@outlook.fr>
2018-04-16 11:25:43 +0200

    Ajout des models

--- AppFilRougeLibrary/FilRouge.Web/Models/QuestionModel.cs
+++ AppFilRougeLibrary/FilRouge.Web/Models/QuestionModel.cs
@@ -1,55 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.ComponentModel;
+//using FilRouge.Model.Entities;
 
-namespace FilRouge.Entities.Model
-{
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
+//namespace FilRouge.Model.Models
+//{
+//    using System.ComponentModel.DataAnnotations;
 
-    using FilRouge.Model.Entities;
+//    public class QuestionModel
+//    {
+//        public int QuestionId { get; set; }
+//        //[Display(Name = "Question", Prompt = "Entrez une question", Description = "Question unique ou multiple")]
+//        [DisplayName("Question")]
+//        [MinLength(1)]
+//        [MaxLength(250)]
+//        //Aussi annoté dans le code first de la base de données
+//        public string Content { get; set; }
+//        public string Comment { get; set; }
+//        public string Type { get; set; }
+//        public List<Response> Reponses { get; set; }
 
-    public class QuestionModel
-    {
-        public int QuestionId { get; set; }
-        [DisplayName("Question:")]
-        public string Content { get; set; }
-        public string Comment { get; set; }
-        public bool Active { get; set; }
-    }
+//    }
+//    //Classe partielle Map servant à passer d'un viewModel à un Model et inversement
+//    public static partial class Map
+//    {
+//        public static QuestionModel MapToQuestionViewModelFull(this Question question)
+//        {
+//            var questionVM = new QuestionModel();
 
-    public static partial class Map
-    {
-        public static QuestionModel MapToQuestionModel(this Question question)
-        {
-            var questionModel = new QuestionModel();
-            if (question == null)
-            {
-                return questionModel;
-            }
+//            if (question == null)
+//                return questionVM;
 
-            questionModel = new QuestionModel
-             {
-                    Content = question.Content
-             };
-            return questionModel;
-        }
+//            questionVM.Reponses = new List<Response>();
 
-        public static Question MapToQuestion(this QuestionModel questionModel)
-        {
-            var question = new Question();
-            if (questionModel == null)
-            {
-                return question;
-            }
+//            questionVM.QuestionId = question.QuestionId;
+//            questionVM.Content = question.Content;
+//            questionVM.Reponses = question.Responses.ToList();
+//            questionVM.Type = question.QuestionTypeId.ToString();
 
-            question = new Question
-                                {
-                                    Content = questionModel.Content
-            };
-            return question;
-        }
-    }
-}
+//            return questionVM;
+//        }
+
+//        public static QuestionModel MapToQuestionModel(this Question question)
+//        {
+//            var questionVM = new QuestionModel();
+
+//            if (question == null)
+//                return questionVM;
+
+//            questionVM.QuestionId = question.QuestionId;
+//            questionVM.Content = question.Content;
+//            questionVM.Type = question.TypeQuestion.NameType;
+//            return questionVM;
+//        }
+
+
+
+//        public static Question MapToQuestion(this QuestionModel questionVM)
+//        {
+//            var question = new Question();
+//            if (questionVM == null)
+//            {
+//                return question;
+//            }
+//            question.Content = questionVM.Content;
+//            question.QuestionTypeId = Int32.Parse(questionVM.Type);
+
+//            return question;
+//        }
+//    }
+//}
