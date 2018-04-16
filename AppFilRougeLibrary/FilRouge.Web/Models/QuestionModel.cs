f57525b5e69287d71e7ce45db5eae9f2eecd37e0
Fouque Nicolas <n.fouque@outlook.fr>
2018-04-16 11:25:43 +0200

    Ajout des models

--- AppFilRougeLibrary/FilRouge.Web/Models/QuestionModel.cs
+++ AppFilRougeLibrary/FilRouge.Web/Models/QuestionModel.cs
@@ -1,75 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
-using FilRouge.Model.Entities;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.ComponentModel;
+//using FilRouge.Model.Entities;
 
-namespace FilRouge.Model.Models
-{
-    using System.ComponentModel.DataAnnotations;
+//namespace FilRouge.Model.Models
+//{
+//    using System.ComponentModel.DataAnnotations;
 
-    public class QuestionModel
-    {
-        public int QuestionId { get; set; }
-        //[Display(Name = "Question", Prompt = "Entrez une question", Description = "Question unique ou multiple")]
-        [DisplayName("Question")]
-        [MinLength(1)]
-        [MaxLength(250)]
-        //Aussi annoté dans le code first de la base de données
-        public string Content { get; set; }
-        public string Comment { get; set; }
-        public string Type { get; set; }
-        public List<Response> Reponses { get; set; }
+//    public class QuestionModel
+//    {
+//        public int QuestionId { get; set; }
+//        //[Display(Name = "Question", Prompt = "Entrez une question", Description = "Question unique ou multiple")]
+//        [DisplayName("Question")]
+//        [MinLength(1)]
+//        [MaxLength(250)]
+//        //Aussi annoté dans le code first de la base de données
+//        public string Content { get; set; }
+//        public string Comment { get; set; }
+//        public string Type { get; set; }
+//        public List<Response> Reponses { get; set; }
 
-    }
-    //Classe partielle Map servant à passer d'un viewModel à un Model et inversement
-    public static partial class Map
-    {
-        public static QuestionModel MapToQuestionViewModelFull(this Question question)
-        {
-            var questionVM = new QuestionModel();
+//    }
+//    //Classe partielle Map servant à passer d'un viewModel à un Model et inversement
+//    public static partial class Map
+//    {
+//        public static QuestionModel MapToQuestionViewModelFull(this Question question)
+//        {
+//            var questionVM = new QuestionModel();
 
-            if (question == null)
-                return questionVM;
+//            if (question == null)
+//                return questionVM;
 
-            questionVM.Reponses = new List<Response>();
+//            questionVM.Reponses = new List<Response>();
 
-            questionVM.QuestionId = question.QuestionId;
-            questionVM.Content = question.Content;
-            questionVM.Reponses = question.Responses.ToList();
-            questionVM.Type = question.QuestionTypeId.ToString();
+//            questionVM.QuestionId = question.QuestionId;
+//            questionVM.Content = question.Content;
+//            questionVM.Reponses = question.Responses.ToList();
+//            questionVM.Type = question.QuestionTypeId.ToString();
 
-            return questionVM;
-        }
+//            return questionVM;
+//        }
 
-        public static QuestionModel MapToQuestionModel(this Question question)
-        {
-            var questionVM = new QuestionModel();
+//        public static QuestionModel MapToQuestionModel(this Question question)
+//        {
+//            var questionVM = new QuestionModel();
 
-            if (question == null)
-                return questionVM;
+//            if (question == null)
+//                return questionVM;
 
-            questionVM.QuestionId = question.QuestionId;
-            questionVM.Content = question.Content;
-            questionVM.Type = question.TypeQuestion.NameType;
-            return questionVM;
-        }
+//            questionVM.QuestionId = question.QuestionId;
+//            questionVM.Content = question.Content;
+//            questionVM.Type = question.TypeQuestion.NameType;
+//            return questionVM;
+//        }
 
 
 
-        public static Question MapToQuestion(this QuestionModel questionVM)
-        {
-            var question = new Question();
-            if (questionVM == null)
-            {
-                return question;
-            }
-            question.Content = questionVM.Content;
-            question.QuestionTypeId = Int32.Parse(questionVM.Type);
+//        public static Question MapToQuestion(this QuestionModel questionVM)
+//        {
+//            var question = new Question();
+//            if (questionVM == null)
+//            {
+//                return question;
+//            }
+//            question.Content = questionVM.Content;
+//            question.QuestionTypeId = Int32.Parse(questionVM.Type);
 
-            return question;
-        }
-    }
-}
+//            return question;
+//        }
+//    }
+//}
--- AppFilRougeLibrary/FilRouge.Web/Models/QuizzModel.cs
+++ AppFilRougeLibrary/FilRouge.Web/Models/QuizzModel.cs
@@ -1,64 +1,64 @@
-﻿using System.ComponentModel;
-using FilRouge.Model.Entities;
+﻿//using System.ComponentModel;
+//using FilRouge.Model.Entities;
 
-namespace FilRouge.Model.Models
-{
-    using System;
-    using System.ComponentModel.DataAnnotations;
+//namespace FilRouge.Model.Models
+//{
+//    using System;
+//    using System.ComponentModel.DataAnnotations;
 
-    public class QuizzModel
-    {
-        public int StateQuizz { get; set; }
-        public string Technology { get; set; }
-        public int QuestionCount { get; set; }
-        [DisplayName("Lastname")]
-        [MinLength(1)]
-        [MaxLength(20)]
-        public string UserLastname { get; set; } // Nom
-        [DisplayName("Firstname")]
-        [MinLength(1)]
-        [MaxLength(20)]
-        public string UserFirstname { get; set; } // Prénom
-        public DateTime DateQuizz { get; set; }
-    }
-    //Classe partielle Map servant à passer d'un viewModel à un Model et inversement
-    public static partial class Map
-    {
-        public static QuizzModel MapToQuizzModel(this Quizz Quizz)
-        {
-            var QuizzModel = new QuizzModel();
-            if (Quizz == null)
-            {
-                return QuizzModel;
-            }
+//    public class QuizzModel
+//    {
+//        public int StateQuizz { get; set; }
+//        public string Technology { get; set; }
+//        public int QuestionCount { get; set; }
+//        [DisplayName("Lastname")]
+//        [MinLength(1)]
+//        [MaxLength(20)]
+//        public string UserLastname { get; set; } // Nom
+//        [DisplayName("Firstname")]
+//        [MinLength(1)]
+//        [MaxLength(20)]
+//        public string UserFirstname { get; set; } // Prénom
+//        public DateTime DateQuizz { get; set; }
+//    }
+//    //Classe partielle Map servant à passer d'un viewModel à un Model et inversement
+//    public static partial class Map
+//    {
+//        public static QuizzModel MapToQuizzModel(this Quizz Quizz)
+//        {
+//            var QuizzModel = new QuizzModel();
+//            if (Quizz == null)
+//            {
+//                return QuizzModel;
+//            }
 
-            QuizzModel = new QuizzModel
-             {
-                 QuestionCount = Quizz.QuestionCount,
-                 Technology = Quizz.Technology.TechnoName,
-                 UserFirstname = Quizz.UserFirstName,
-                 UserLastname = Quizz.UserLastName,
-                 //DateQuizz = Quizz.
-            };
-            return QuizzModel;
+//            QuizzModel = new QuizzModel
+//             {
+//                 QuestionCount = Quizz.QuestionCount,
+//                 Technology = Quizz.Technology.TechnoName,
+//                 UserFirstname = Quizz.UserFirstName,
+//                 UserLastname = Quizz.UserLastName,
+//                 //DateQuizz = Quizz.
+//            };
+//            return QuizzModel;
 
-        }
+//        }
 
-        public static Quizz MapToQuizz(this QuizzModel QuizzModel)
-        {
-            var Quizz = new Quizz();
-            if (QuizzModel == null)
-            {
-                return Quizz;
-            }
+//        public static Quizz MapToQuizz(this QuizzModel QuizzModel)
+//        {
+//            var Quizz = new Quizz();
+//            if (QuizzModel == null)
+//            {
+//                return Quizz;
+//            }
 
-            Quizz = new Quizz
-            {
-                UserLastName = QuizzModel.UserLastname,
-                UserFirstName = QuizzModel.UserFirstname,
-                QuestionCount = QuizzModel.QuestionCount
-            };
-            return Quizz;
-        }
-    }
-}
+//            Quizz = new Quizz
+//            {
+//                UserLastName = QuizzModel.UserLastname,
+//                UserFirstName = QuizzModel.UserFirstname,
+//                QuestionCount = QuizzModel.QuestionCount
+//            };
+//            return Quizz;
+//        }
+//    }
+//}
