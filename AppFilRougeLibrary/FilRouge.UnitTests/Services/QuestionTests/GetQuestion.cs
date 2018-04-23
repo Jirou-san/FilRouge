1a3601dda1d311faf0e35a8863969de20efe4062
Fouque Nicolas <n.fouque@outlook.fr>
2018-04-23 22:58:01 +0200

    Petites modifs sur les tests unitaires

---
+++ AppFilRougeLibrary/FilRouge.UnitTests/Services/QuestionTests/GetQuestion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FilRouge.Model.Entities;
+using System.Collections.Generic;
+
+namespace FilRouge.UnitTests.Services.QuestionTests
+{
+    /// <summary>
+    /// Classe d'instanciation d'objets propre aux questions
+    /// </summary>
+    [TestClass]
+    public class GetQuestion
+    {
+        //TODO: Les autres tests
+        
+        [TestMethod]
+        public void TestGetQuestion()
+        {
+            
+
+            var questionHandler = new QuestionReference();
+            var reference = TestReference.QuestionResponseService();
+            Assert.IsNotNull(questionHandler.NewQuestion());
+            //Bug bizzare
+            Assert.AreSame(questionHandler.NewQuestion(), typeof(int));
+
+        }
+    }
+}
---
+++ AppFilRougeLibrary/FilRouge.UnitTests/TestReference.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FilRouge.Service;
+using FilRouge.Model.Entities;
+namespace FilRouge.UnitTests
+{
+    /// <summary>
+    /// Nécessaire pour l'environnement de test
+    /// </summary>
+    public static class TestReference
+    {
+        /// <summary>
+        /// Création d'un dbcontext, à utiliser dans un using
+        /// </summary>
+        /// <returns></returns>
+        ///
+
+        
+        public static QuestionResponseService QuestionResponseService()
+        {
+            return new QuestionResponseService(new FilRougeDBContext());
+        }
+        public static ReferencesService ReferenceService()
+        {
+            return new ReferencesService(new FilRougeDBContext());
+        }
+        public static QuizzService QuizzService()
+        {
+            //TODO: Rejouter un paramètre db context dans le constructeur
+            //Pour optimiser la réutilisabilité du code et son ergonomie
+            return new QuizzService();
+        }
+    }
+
+    /// <summary>
+    /// Classe de référence pour les questions ainsi que la création d'objets nécessaire aux tests
+    /// </summary>
+    public class QuestionReference
+    {
+        public QuestionReference()
+        {
+
+        }
+        public int NewQuestion()
+        {
+            Question question = new Question()
+            {
+                DifficultyId = 1,
+                Content = "Unit test",
+                IsFreeAnswer = true,
+                IsEnable = true,
+                Responses = new List<Response>(),
+                TechnologyId = 1,
+            };
+
+            return TestReference.QuestionResponseService().AddQuestion(question);
+        }
+    }
+}
