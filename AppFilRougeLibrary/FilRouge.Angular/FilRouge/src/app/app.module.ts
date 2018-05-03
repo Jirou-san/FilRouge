df0f7727b419132e06c7a8501245727b50fbb53c
Fouque Nicolas <n.fouque@outlook.fr>
2018-05-03 14:59:08 +0200

    Modifications HTML

--- AppFilRougeLibrary/FilRouge.Angular/FilRouge/src/app/app.module.ts
+++ AppFilRougeLibrary/FilRouge.Angular/FilRouge/src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { QuizzComponent } from './quizz/quizz.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { QuestionsreponsesComponent } from './questionsreponses/questionsreponses.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+const appRoutes: Routes = [
+  {
+    path: 'quizz', component: QuizzComponent
+  },
+  {
+    path: '', component: AppComponent
+  },
+  {
+    path: 'doquizz', component: QuestionsreponsesComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizzComponent
+    QuizzComponent,
+    QuestionsreponsesComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
   // tslint:disable-next-line:no-trailing-whitespace
   
---
+++ AppFilRougeLibrary/FilRouge.Angular/FilRouge/src/app/questionsreponses/questionsreponses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionsreponses',
+  templateUrl: './questionsreponses.component.html',
+  styleUrls: ['./questionsreponses.component.scss']
+})
+export class QuestionsreponsesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
