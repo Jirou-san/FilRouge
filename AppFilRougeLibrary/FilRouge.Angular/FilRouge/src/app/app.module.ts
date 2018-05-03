2e03fb191a60743958e73435515e66a25412505c
Fouque Nicolas <n.fouque@outlook.fr>
2018-05-03 12:16:42 +0200

    First push

--- AppFilRougeLibrary/FilRouge.Angular/FilRouge/src/app/app.module.ts
+++ AppFilRougeLibrary/FilRouge.Angular/FilRouge/src/app/app.module.ts
@@ -1,17 +1,21 @@
+import { QuizzComponent } from './quizz/quizz.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizzComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
+  // tslint:disable-next-line:no-trailing-whitespace
+  
   providers: [],
   bootstrap: [AppComponent]
 })
